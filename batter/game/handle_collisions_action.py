331c7f6b9ff15e088febdb4573d5b7bf25ba69c9
Braiden Sommer <braidensommer@yahoo.com>
2021-11-03 15:40:09 -0600

    Starter code

---
+++ batter/game/handle_collisions_action.py
@@ -0,0 +1,12 @@
+import random
+from game import constants
+from game.action import Action
+
+class HandleCollisionsAction(Action):
+    """A code template for handling collisions. The responsibility of this class of objects is to update the game state when actors collide.
+    
+    Stereotype:
+        Controller
+    """
+
+    
